25f6e1376a4e7e64d2c9ebd3816426542fd37198
marcograsso <56439271+marcograsso@users.noreply.github.com>
2020-10-25 18:15:06 +0100

    Update metadata_viewer.js

--- tech/metadata_viewer.js
+++ tech/metadata_viewer.js
@@ -557,3 +557,15 @@ $(document).ready(function(){
         $("#2040eye").show();
   });
 });
+
+//Manage cover images
+$(document).ready (function(){
+  if(!$("div").hasClass("coverpic")) {
+ $( ".title").toggleClass('titlenoimage newClass');
+}
+});
+
+$(document).ready(function(){
+        $('.articleImg[src=""]').hide();
+        $('.articleImg:not([src=""])').show();
+    });
