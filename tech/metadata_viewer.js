f8de695661302401084cf430dd2460f261c98ada
sntcristian <snt.cristian@gmail.com>
2020-11-04 11:35:13 +0100

    minfix

--- tech/metadata_viewer.js
+++ tech/metadata_viewer.js
@@ -1,8 +1,8 @@
-window.onload = function(){ document.getElementById('loading').style.display = 'none'; }
+window.onload = function(){ setTimeout(function() {document.getElementById('loading').style.display = 'none';}, 4800) };
 
 $(document).on({
     ajaxStart: function() { document.getElementById('loading').style.display = 'block';    },
-    ajaxStop: function() { document.getElementById('loading').style.display = 'none'; }    
+    ajaxStop: function(){ setTimeout(function() {document.getElementById('loading').style.display = 'none';}, 4800) }; 
 });
 
 window.onscroll = function() {myFunction()};
@@ -584,7 +584,7 @@ function changeStyle(selectedStyle) {
     cssFile = document.createElement('link');
     cssFile.type = "text/css"; 
     cssFile.rel = "stylesheet";
-    cssFile.onload = function(){ document.getElementById('loading').style.display = 'none'; }
+    cssFile.onload = function(){ setTimeout(function() {document.getElementById('loading').style.display = 'none';}, 4800) };
     cssFile.href = selectedStyle;
     cssFile.id= "ArticleCss";
     document.getElementsByTagName("head")[0].appendChild(cssFile);
